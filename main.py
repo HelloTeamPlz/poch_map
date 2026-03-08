9f7215aa81ff33c9a317f0dee1e409018491e4c7
Thomas <mrtommynell@gmail.com>
2026-03-08 14:53:00 -0500

    fix for space to -

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ async def p(ctx, *, system_name):
   """
   with open('sys.txt') as file:
     contents = file.read()
-    search_word = system_name.lower()
+    search_word = system_name.lower().replace(" ", "-")
     if search_word in contents:
         file.close()
         embed = discord.Embed(description=f"Here's the exit map for {system_name}")
